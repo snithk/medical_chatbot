04d1cc11bf6099a574ce34c58f7d95f1ed856469
lokasnithik@gmail.com <your-email@example.com>
2025-09-09 00:10:50 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+# create a template script
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch README.md
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "directory structure created."
